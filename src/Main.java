e00e857fc28972db956b8a3cd3161d23c7b8d3cc
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2024-01-31 09:43:14 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if(N % 2 > 0 || N > 5 && N < 21)
+        {
+            System.out.println("Weird");
+        }
+        else if(N % 2 == 0  && N > 1 && N < 6 || N > 20)
+        {
+            System.out.println("Not Weird");
+        }
+
+        scanner.close();
+    }
+}
